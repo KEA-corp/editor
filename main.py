e8fb328f6f6faea8fbd2dc863a56bba7a7ca88d8
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-01-20 20:56:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
+from errno import errorcode
 import tkinter as tk
-from turtle import color
 
 global old_dim, MODS
 geometry = "1050x700"
 old_dim = iter(geometry.split("x"))
+
+MODS_DOC = {
+    1: "variable de sortie",
+    2: "variable d'entrée",
+    3: "nom de boucle",
+    4: "comparateur",
+    5: "opérateur",
+    6: "mode de debug",
+    7: "texte/valleur",
+    8: "nom de fonction",
+
+}
+
 MODS = {
-    "A": "yellow",
-    "B": "red",
-    "C": "red",
-    "D": "blue",
-    "E": "purple",
-    "F": "green",
-    "H": "red",
-    "I": "red",
-    "L": "purple",
-    "R": "red",
-    "S": "yellow",
-    "T": "purple",
-    "V": "red",
-    "X": "purple",
-    "Z": "purple",
+    "A": ("yellow", (2)),
+    "B": ("red",    (1,2,4,2)),
+    "C": ("red",    (1,2,5,2)),
+    "D": ("cyan",   (6)),
+    "E": ("purple", (3)),
+    "F": ("blue",   (8)),
+    "H": ("red",    (1,2)),
+    "I": ("red",    (1)),
+    "L": ("purple", (3,2)),
+    "R": ("red",    (1,2)),
+    "S": ("yellow", (7)),
+    "T": ("purple", (8)),
+    "V": ("red",    (1,7)),
+    "X": ("purple", (3,1)),
+    "Z": ("purple", ()),
+    "//": ("white", ()),
 }
 
 fenetre = tk.Tk()
@@ -54,10 +68,23 @@ def actu():
         old_dim = new_dim
     text = get_text()
     for i in range(len(text)):
-        mod = text[i].replace("\t", "").strip().split(" ")[0]
+        arg = text[i].replace("\t", "").strip().split(" ")
+        mod = arg[0]
         if mod in MODS.keys():
             ZCODE.tag_add(mod, f"{i+1}.0", f"{i+1}.1")
-            ZCODE.tag_config(mod, foreground=MODS[mod])
+            if len(MODS[mod][1]) > 0 and len(MODS[mod][1]) + 1 != len(arg):
+                BGcode = "er1"
+            elif mod == "//":
+                BGcode = "comment"
+            else:
+                BGcode = "no"
+
+            ZCODE.tag_add(BGcode, f"{i+1}.0", f"{i+1}.{len(text[i])}")
+            ZCODE.tag_config(mod, foreground=MODS[mod][0])
+
+    ZCODE.tag_config("er1", background="#554400")        # le nombre d'arguments est incorrect
+    ZCODE.tag_config("comment", background="#004400")    # commentaire
+    ZCODE.tag_config("no", background="#0C0F1D")         # pas d'erreur
 
 
 
