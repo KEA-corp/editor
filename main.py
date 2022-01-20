e6c45fdc57e720b7e5e761e81d0b2ede840e9821
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-01-20 21:24:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ MODS_DOC = {
 }
 
 MODS = {
-    "A": ("yellow", (2)),
-    "B": ("red",    (1,2,4,2)),
-    "C": ("red",    (1,2,5,2)),
-    "D": ("cyan",   (6)),
-    "E": ("purple", (3)),
-    "F": ("blue",   (8)),
-    "H": ("red",    (1,2)),
-    "I": ("red",    (1)),
-    "L": ("purple", (3,2)),
-    "R": ("red",    (1,2)),
-    "S": ("yellow", (7)),
-    "T": ("purple", (8)),
-    "V": ("red",    (1,7)),
-    "X": ("purple", (3,1)),
-    "Z": ("purple", ()),
-    "//": ("white", ()),
+    "A": ("yellow", [2]),
+    "B": ("red",    [1,2,4,2]),
+    "C": ("red",    [1,2,5,2]),
+    "D": ("cyan",   [6]),
+    "E": ("purple", [3]),
+    "F": ("blue",   [8]),
+    "H": ("red",    [1,2]),
+    "I": ("red",    [1]),
+    "L": ("purple", [3,2]),
+    "R": ("red",    [1,2]),
+    "S": ("yellow", []),
+    "T": ("purple", [8]),
+    "V": ("red",    [1,7]),
+    "X": ("purple", [3,1]),
+    "Z": ("purple", []),
+    "//": ("white", []),
 }
 
 fenetre = tk.Tk()
@@ -57,7 +57,7 @@ def place_editor():
     ZCODE.place(x=0, y=0, width=x, height=y)
 
 def get_text():
-    return ZCODE.get("1.0", tk.END).split("\n")[:-1]
+    return [l.replace("\t", "").replace("    ", "") for l in ZCODE.get("1.0", tk.END).split("\n")[:-1]]
 
 def actu():
     global old_dim
@@ -68,25 +68,24 @@ def actu():
         old_dim = new_dim
     text = get_text()
     for i in range(len(text)):
-        arg = text[i].replace("\t", "").strip().split(" ")
+        arg = text[i].split(" ")
         mod = arg[0]
         if mod in MODS.keys():
             ZCODE.tag_add(mod, f"{i+1}.0", f"{i+1}.1")
             if len(MODS[mod][1]) > 0 and len(MODS[mod][1]) + 1 != len(arg):
-                BGcode = "er1"
+                BGcode = f"{i}er1"
             elif mod == "//":
-                BGcode = "comment"
+                BGcode = f"{i}comment"
             else:
                 BGcode = "no"
-
-            ZCODE.tag_add(BGcode, f"{i+1}.0", f"{i+1}.{len(text[i])}")
+                for _ in [f"{i}er1", f"{i}comment"]:
+                    ZCODE.tag_delete(_)
+            if BGcode != "no":
+                ZCODE.tag_add(BGcode, f"{i+1}.0", f"{i+1}.{len(text[i])}")
             ZCODE.tag_config(mod, foreground=MODS[mod][0])
 
-    ZCODE.tag_config("er1", background="#554400")        # le nombre d'arguments est incorrect
-    ZCODE.tag_config("comment", background="#004400")    # commentaire
-    ZCODE.tag_config("no", background="#0C0F1D")         # pas d'erreur
-
-
+            ZCODE.tag_config(f"{i}er1", background="#554400")        # le nombre d'arguments est incorrect
+            ZCODE.tag_config(f"{i}comment", background="#004400")    # commentaire
 
     fenetre.after(250, actu)
 
